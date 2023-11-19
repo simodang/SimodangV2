e304cf211263f562a0f32a306188ad72e8db44e6
Aldi-H <aldihimawan88@gmail.com>
2023-11-19 21:22:49 +0700

    chore: configure react-native-paper

--- index.js
+++ index.js
@@ -2,8 +2,18 @@
  * @format
  */
 
+import React from 'react';
 import { AppRegistry } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
 import App from './App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+export const Main = () => {
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => Main);
