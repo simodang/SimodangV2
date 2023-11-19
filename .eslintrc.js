34ff98b349ea9ecd6088cf3bc70cc57f74b89ba3
Aldi-H <aldihimawan88@gmail.com>
2023-11-19 19:50:07 +0700

    chore: add configuration for linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    // ...
+    'prettier/prettier': ['error', { singleQuote: true, bracketSpacing: true }],
+    quotes: ['error', 'single', 'avoid-escape'],
+    'react/no-unstable-nested-components': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+  },
 };
