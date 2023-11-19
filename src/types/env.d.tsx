e6934c9e31171bba449abb4118b9949c8bc07340
Aldi-H <aldihimawan88@gmail.com>
2023-11-19 19:51:07 +0700

    chore: add configuration for env

---
+++ src/types/env.d.tsx
@@ -0,0 +1,7 @@
+declare module '@env' {
+  export const WEBCLIENT_ID: string;
+  export const SCOPES_USERINFO_EMAIL: string;
+  export const SCOPES_USERINFO_PROFILE: string;
+  export const BASE_URL: string;
+  export const SOCKET_URL: string;
+}
